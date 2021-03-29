0ae0341e983a6942ced6fcf1acc8a3e0d73fb891
UrbanRaccoon <81574169+UrbanRaccoon@users.noreply.github.com>
2021-03-29 22:40:00 +0200

    Update LaravizServiceProvider.php

--- src/Providers/LaravizServiceProvider.php
+++ src/Providers/LaravizServiceProvider.php
@@ -15,7 +15,7 @@
 
 namespace Vizrex\Laraviz;
 
-use Vizrex\Laraviz\Providers\BaseServiceProvider;
+use \Vizrex\Laraviz\Providers\BaseServiceProvider;
 
 class LaravizServiceProvider extends BaseServiceProvider
 {
